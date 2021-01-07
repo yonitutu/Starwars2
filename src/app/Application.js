94aba6a002e7cd3c51223c13cd1f285100ca66ce
Aleksandar Vlaskovski <a.vlaskovski@dopamine.bg>
2021-01-07 09:37:00 +0200

    feat: add eventemitter3 to boilerplate

--- src/app/Application.js
+++ src/app/Application.js
@@ -1,24 +1,38 @@
 import config from '../config';
+import EventEmitter from 'eventemitter3';
+
+const EVENTS = {
+  APP_READY: 'app_ready',
+};
 
 /**
  * App entry point.
  * All configurations are described in src/config.js
  */
-export default class Application {
+export default class Application extends EventEmitter {
   constructor() {
+    super();
+
     this.config = config;
-    this.data = null;
+    this.data = {};
 
     this.init();
+  }
+
+  static get events() {
+    return EVENTS;
   }
 
   /**
    * Initializes the app.
    * Called when the DOM has loaded. You can initiate your custom classes here
-   * and manipulate the DOM tree.
+   * and manipulate the DOM tree. Task data should be assigned to Application.data.
+   * The APP_READY event should be emitted at the end of this method.
    */
   async init() {
+    // Initiate classes and wait for async operations here.
 
+    this.emit(Application.events.APP_READY);
   }
 }
 
